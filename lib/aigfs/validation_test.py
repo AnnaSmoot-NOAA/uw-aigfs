8c0f4a51c882eec10d85a1e739b554972be51d6e
Paul Madden <136389411+maddenp-cu@users.noreply.github.com>
2026-08-28 09:15:02 -0600

    Various fixes (#66)

--- lib/aigfs/validation_test.py
+++ lib/aigfs/validation_test.py
@@ -9,7 +9,7 @@ from aigfs import validation
 @fixture
 def args_app(args_platform, args_time, tmp_path, utc):
     return dict(
-        cycle_freq=timedelta(hours=1),
+        cycle_freq=timedelta(hours=12),
         first_cycle=utc(2026, 1, 1, 0),
         home=tmp_path,
         last_cycle=utc(2026, 1, 31, 23),
@@ -46,7 +46,16 @@ def args_scheduler():
 
 @fixture
 def args_time():
-    return dict(fff="fff", hh="hh", yyyymmdd="yyyymmdd")
+    return dict(
+        fff="006",
+        hh="00",
+        m1_hh="18",
+        m1_yyyymmdd="20260826",
+        m2_hh="12",
+        m2_yyyymmdd="20260826",
+        m6h=timedelta(hours=6),
+        yyyymmdd="20260827",
+    )
 
 
 @mark.parametrize("compute", ["a", None])
@@ -115,9 +124,15 @@ def test_validation_App(args_app, with_del):
 
 
 @mark.parametrize("hours", [0, -1])
-def test_validation_App_bad_cycle_freq(args_app, hours):
+def test_validation_App_bad_cycle_freq_negative(args_app, hours):
     args_app["cycle_freq"] = timedelta(hours=hours)
     with raises(ValueError, match="cycle_freq must be greater than 0"):
+        validation.App(**args_app)
+
+
+def test_validation_App_bad_cycle_freq_not_0_mod_6(args_app):
+    args_app["cycle_freq"] = timedelta(hours=1)
+    with raises(ValueError, match="cycle_freq must be a multiple of 6"):
         validation.App(**args_app)
 
 
